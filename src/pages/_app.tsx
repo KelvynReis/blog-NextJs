38972ea2b3cf337669e87501fa4b3ec3ef26c70e
kelvyn <kelvyn2reis@gmail.com>
2022-08-12 11:51:44 -0300

    add styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/global-styles.ts';
+import type { AppProps } from 'next/app';
+import { GlobalStyles } from '../styles/global-styles';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </ThemeProvider>
+  );
 }
-
-export default MyApp
