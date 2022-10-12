1d24ff11894ec06ed53687512e33097a5b772ce0
Kelvyn Reis <kelvyn2reis@gmail.com>
2022-10-12 17:39:57 -0300

    corrected query

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,5 +1,5 @@
 export const API_URL = 'http://localhost:1337/api';
 export const POSTS_URL = `http://localhost:1337/api/posts`;
-export const URL_WITH_POLULATE = '?polulate=*';
+export const URL_WITH_POLULATE = 'populate=*';
 
 export const SITE_NAME = 'My Blog Posts';
